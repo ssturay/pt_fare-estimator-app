7e87ff12d64fef289d08b3fc256c850c162f6bc0
ssturay <127793300+ssturay@users.noreply.github.com>
2025-07-14 18:28:15 +0000

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -28,7 +28,7 @@ lang = st.sidebar.selectbox("🌐 Choose Language", ["English", "Krio"])
 
 L = {
     "English": {
-        "title": "🚖 Dynamic Public Transport Fare Estimator – Freetown",
+        "title": "🚖 Freetown Public Transport Fare Estimator (FEsApp)",
         "vehicle_type": "Select vehicle type",
         "distance": "Enter distance (km)",
         "fuel_price": "Enter current fuel price (SLL)",
@@ -56,7 +56,7 @@ L = {
         "your_location": "📍 Your current location"
     },
     "Krio": {
-        "title": "🚖 Transpɔt Fɛr Estimeita – Fritɔn",
+        "title": "🚖 Fritɔn Transpɔt Fɛr Estimator",
         "vehicle_type": "Chuz di kaind transpɔt",
         "distance": "Wos dɔns (km)",
         "fuel_price": "Put di fɔyəl pris (SLL)",
