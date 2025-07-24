c6e3672353adaf432ac186b372229385b18dd476
Simeon Stevenson TURAY <127793300+ssturay@users.noreply.github.com>
2025-07-24 18:12:54 +0000

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -62,7 +62,7 @@ lang = st.sidebar.selectbox("🌐 Choose Language", ["English", "Krio"])
 
 L = {
     "English": {
-        "title": "🚖 Freetown Public Transport Fare Estimator - FEsApp",
+        "title": "🚖 Freetown Public Transport Fare Estimator (FEsApp) 🇸🇱",
         "vehicle_type": "Select vehicle type",
         "distance": "Enter distance (km)",
         "fuel_price": "Enter current fuel price (SLL)",
@@ -90,7 +90,7 @@ L = {
         "your_location": "📍 Your current location"
     },
     "Krio": {
-        "title": "🚖 Fritɔn Transpɔt Fɛr Estimator - FEsApp",
+        "title": "🚖 Fritɔn Transpɔt Fɛr Estimator (FEsApp) 🇸🇱",
         "vehicle_type": "Chuz di kaind transpɔt",
         "distance": "Wos dɔns (km)",
         "fuel_price": "Put di fɔyəl pris (SLL)",
